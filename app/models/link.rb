4ed47ee9c959534a7b4c87ec01d26eb86f766b0e
Siddharth Shringi <siddharthshringi@gmail.com>
2020-05-12 10:58:28 +0530

    Added model link test cases - Fixed the link fixture file - Added following test cases   - short_url should be unique   - short_url should be 8 character

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,5 @@
 class Link < ApplicationRecord
   
   validates :url, presence: true, format: URI::regexp(%w[http https])
-  validates :short_url, uniqueness: true
+  validates :short_url, uniqueness: true, length: { is: 8 }
 end
--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -15,4 +15,16 @@ class LinkTest < ActiveSupport::TestCase
     assert_not @link.valid?
     assert_equal ["Url is invalid"], @link.errors.full_messages
   end
+
+  test "short_url should be unique" do
+    @link.short_url = links(:one).short_url
+    assert_not @link.valid?
+    assert_equal ["Short url has already been taken"], @link.errors.full_messages
+  end
+
+  test "short_url should be 8 character" do
+    @link.short_url = "TPMHWCBKHPY"
+    assert_not @link.valid?
+    assert_equal ["Short url is the wrong length (should be 8 characters)"], @link.errors.full_messages
+  end
 end
