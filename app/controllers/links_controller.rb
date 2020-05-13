e7e3062d196ca3d532edd63500f883f97f51e229
Siddharth Shringi <siddharthshringi@gmail.com>
2020-05-13 13:17:21 +0530

    Fixed following changes - Fixed record_not_found function - Fixed create action in links_controller - Added test case "should not create link if it already exist"

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,18 +1,22 @@
 class LinksController < ApplicationController
 
   def show
-    @link = Link.find_by(short_url: params[:short_url])
+    @link = Link.find_by!(short_url: params[:short_url])
     render status: :ok, json: { url: @link.url }
 
   end
 
   def create
-    @link = Link.new(link_params)
-    @link.short_url = @link.generate_short_url
-    if @link.save
-      render status: :ok, json: { link: @link }
+    @link = Link.find_or_initialize_by(link_params)
+    if @link.new_record?
+      @link.short_url = @link.generate_short_url
+      if @link.save
+        render status: :ok, json: { short_url: @link.short_url }
+      else
+        render status: :unprocessable_entity, json: { errors: @link.errors.full_messages }
+      end
     else
-      render status: :unprocessable_entity, json: { errors: @link.errors.full_messages }
+      render status: :ok, json: { short_url: @link.short_url }
     end
   end
 
--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -4,15 +4,24 @@ class LinksControllerTest < ActionDispatch::IntegrationTest
 
   test "should be able to create link" do
     assert_difference 'Link.count', 1 do
-      post links_path, params: { link: { url: "https://bigbinary.com/carrer" }}
+      post links_path, params: { link: { url: "https://bigbinary.com/products" }}
     end
     assert_response :ok
+
+  end
+
+  test "should not create link if it already exist?" do
+    assert_difference 'Link.count', 0 do
+      post links_path, params: { link: { url: links(:one).url }}
+    end
+    assert_response :ok
+    assert_equal json_body["short_url"], links(:one).short_url
   end
 
   test "should show original url" do
     get short_url_path(links(:one).short_url)
     assert_response :ok
-    assert_equal "https://bigbinary.com/jobs", json_body["url"]
+    assert_equal links(:one).url, json_body["url"]
   end
 
 end
