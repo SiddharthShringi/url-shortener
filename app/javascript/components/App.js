edc92d3aa60e547bd5def2bae41997a86fa288b2
Siddharth Shringi <siddharthshringi@gmail.com>
2020-05-18 13:14:34 +0530

    - Added react, bootstrap and font-awesome

---
+++ app/javascript/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="text-primary">Bootstrap Working</h1>
+        <p>Start working on the app asap</p>
+        <span className="ml-3">
+          <i class="fas fa-thumbtack"></i>
+        </span>
+      </div>
+    );
+  }
+}
+
+export default App;
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static#index'
   resources :links, only: :create
   resources :links, param: :short_url, only: :show
 end
