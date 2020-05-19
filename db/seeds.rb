b3f03fba3ea37539cd74a24d6a1ef72eee5906f2
Siddharth Shringi <siddharthshringi@gmail.com>
2020-05-19 18:39:31 +0530

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+links_attributes= [
+  {url: "https://bigbinary.com/jobs"},
+  {url: "https://yahoo.com"},
+  {url: "https://hello.com/jobs"},
+  {url: "https://facbook.com/jobs"},
+  {url: "https://google.com/jobs"},
+  {url: "https://hello.com/jobs"},
+  {url: "https://hello.com/jobs"}
+]
+
+
+link_attributes.each do |attributes|
+  Link.create(attributes)
+end
