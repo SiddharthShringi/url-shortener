345c36b700f43a008527cfd354552561ebae975a
Siddharth Shringi <siddharthshringi@gmail.com>
2020-05-19 18:09:25 +0530

    Fix deploy issue

--- db/migrate/20200517144503_add_unique_index_on_short_url.rb
+++ db/migrate/20200517144503_add_unique_index_on_short_url.rb
@@ -1,7 +1,6 @@
 class AddUniqueIndexOnShortUrl < ActiveRecord::Migration[6.0]
   def change
     change_column :links, :short_url, :string, null: false
-    remove_index :links, :short_url
     add_index :links, :short_url, unique: true
   end
 end
